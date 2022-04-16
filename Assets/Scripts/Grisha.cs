0cef72b961176589b7eb6900f85bc9721ced18ed
Alzaar <umbralar@gmail.com>
2022-04-16 10:42:54 +1000

    Climbing tiles update
    Now this ones are in specific tilemap

--- Assets/Scripts/Grisha.cs
+++ Assets/Scripts/Grisha.cs
@@ -5,22 +5,19 @@ using UnityEngine.Tilemaps;
 
 public class Grisha : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] private float speed = 3f;
     [SerializeField] private float ClimbingSpeed = 3f;
     [SerializeField] private float jumpForce = 10f;
     private Rigidbody2D rb;
     public bool isGrounded;
     public bool isRuning;
-    public int climbingTilesAtached;
+    public bool isClimbing;
     private Vector2 climbingDirection;
     private Vector2 runDirection;
     public float baseGravityScale;
 
     private void Run()
     {
-        //Vector3 dir = transform.right * Input.GetAxis("Horizontal");
-        //transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
         if (isRuning)
         {
             transform.Translate(runDirection.normalized * speed);
@@ -29,14 +26,9 @@ public class Grisha : MonoBehaviour
 
     private void Climbing()
     {
-        if (climbingTilesAtached > 0)
+        if (isClimbing)
         {
-            //rb.gravityScale = 0f;
             transform.Translate(climbingDirection.normalized * ClimbingSpeed);
-        }
-        else
-        {
-            //rb.gravityScale = baseGravityScale;
         }
     }
 
@@ -46,7 +38,6 @@ public class Grisha : MonoBehaviour
         runDirection.x = 1;
         climbingDirection.y = 1;
         isRuning = true;
-        climbingTilesAtached = 0;
         baseGravityScale = rb.gravityScale;
     }
 
@@ -54,7 +45,6 @@ public class Grisha : MonoBehaviour
     {
         if (isGrounded)
         {
-            //Debug.Log(isGrounded);
             if (rb.gravityScale > 0)
             {
                 rb.AddForce(Vector2.up * jumpForce);
@@ -66,20 +56,14 @@ public class Grisha : MonoBehaviour
         }
     } 
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    Jump();
-        //}
         Run();
         Climbing();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Debug.Log(collision.gameObject.name);
         if (collision.gameObject.tag == "JumpTrigger")
         {
             Debug.Log("Jump");
@@ -88,11 +72,55 @@ public class Grisha : MonoBehaviour
         }
         else if (collision.gameObject.tag == "GravityReverseTrigger")
         {
-            transform.Rotate(transform.rotation.x + 180, 0, 0, Space.Self);
-            GetComponent<Rigidbody2D>().gravityScale *= -1;
-            GetComponent<Grisha>().baseGravityScale *= -1;
             Debug.Log("Grav");
+            transform.Rotate(transform.rotation.x + 180, 0, 0, Space.Self);
+            rb.gravityScale *= -1;
+            baseGravityScale *= -1;
             Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ClimbingTiles")
+        {
+            if (!isClimbing)
+            {
+                Debug.Log(collision.gameObject.name);
+                float deflectionX = Mathf.Abs(collision.GetContact(0).point.x - collision.GetContact(1).point.x);
+                float deflectionY = Mathf.Abs(collision.GetContact(0).point.y - collision.GetContact(1).point.y);
+                foreach (ContactPoint2D contactPoint2D in collision.contacts)
+                {
+                    if (collision.GetContact(0).point.x - contactPoint2D.point.x > deflectionX)
+                    {
+                        deflectionX = Mathf.Abs(collision.GetContact(0).point.x - contactPoint2D.point.x);
+                    }
+                    if (collision.GetContact(0).point.y - contactPoint2D.point.y > deflectionX)
+                    {
+                        deflectionY = Mathf.Abs(collision.GetContact(0).point.y - contactPoint2D.point.y);
+                    }
+                }
+                if (deflectionY > deflectionX)
+                {
+                    
+                    isClimbing = true;
+                    isRuning = false;
+                    rb.gravityScale = 0f;
+                }
+            }
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ClimbingTiles")
+        {
+            if (isClimbing)
+            {
+                Debug.Log("Climbing off");
+                isClimbing = false;
+                isRuning = true;
+                rb.gravityScale = baseGravityScale;
+            }
         }
     }
 }
--- Assets/Scripts/TriggersStartPosition.cs
+++ Assets/Scripts/TriggersStartPosition.cs
@@ -4,21 +4,12 @@ using UnityEngine;
 
 public class TriggersStartPosition : MonoBehaviour
 {
-    private Grid grid;
-    // Start is called before the first frame update
     void Start()
     {
-        grid = GetComponentInParent<Grid>();
+        Grid grid = GetComponentInParent<Grid>();
         for (int i = 0; i < transform.childCount; i++)
         {
             transform.GetChild(i).transform.position = grid.GetCellCenterWorld(grid.WorldToCell(transform.GetChild(i).transform.position));
         }
-        //transform.position = grid.GetCellCenterWorld(grid.WorldToCell(transform.position));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
